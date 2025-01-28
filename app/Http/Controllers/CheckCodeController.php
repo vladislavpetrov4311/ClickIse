86ab2a6b3deca90a9e77ef21ab9c68d532dc29ba
Vladislav4311 <petrov_vd_4311@mail.ru>
2025-01-28 14:29:39 +0300

    Шаг 10. добавляем route с контроллером для проверки кода

---
+++ app/Http/Controllers/CheckCodeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CheckCodeController extends Controller
+{
+    public function __invoke() {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 use App\Http\Controllers\SelectOptionController;
 use App\Http\Controllers\SetOptionController;
+use App\Http\Controllers\CheckCodeController;
 
 Route::get('/main', SelectOptionController::class);
 Route::post('SetOption', SetOptionController::class);
+Route::post('CheckCode', CheckCodeController::class);
