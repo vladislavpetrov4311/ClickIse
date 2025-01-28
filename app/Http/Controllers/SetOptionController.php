e53b797dc742a1f0df355fb363a1e903650c91bb
Vladislav4311 <petrov_vd_4311@mail.ru>
2025-01-28 11:49:56 +0300

    Шаг 8. Собираем всё в основной контроллер

--- app/Http/Controllers/SetOptionController.php
+++ app/Http/Controllers/SetOptionController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\OptionRequest;
+use App\Service\SelectOption\ServiceGetCode;
+use App\DTO\SelectOptionDTO;
 
 class SetOptionController extends Controller
 {
-    public function __invoke() {
-        return response()->json(["Code" => "generated code"]);
+    public function __invoke(OptionRequest $request, ServiceGetCode $service) {
+        
+        $code = $service->SelectServiceOption(new SelectOptionDTO($request->validated()));
+
+        return response()->json(["Code" => $code]);
     }
 }
