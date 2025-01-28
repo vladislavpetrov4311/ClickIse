f2ebe65e0920730e6a8a57b1bfedd05fc87f2ffb
Vladislav4311 <petrov_vd_4311@mail.ru>
2025-01-28 17:11:18 +0300

    Шаг 15. Собираем всё в контроллер

--- app/Http/Controllers/CheckCodeController.php
+++ app/Http/Controllers/CheckCodeController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CheckCodeRequest;
+use App\DTO\CheckCodeDTO;
+use App\Service\CheckCode\ServiceCheckCode;
 
 class CheckCodeController extends Controller
 {
-    public function __invoke() {
-        
+    public function __invoke(CheckCodeRequest $request, ServiceCheckCode $service) {
+
+       return $service->CheckCode(new CheckCodeDTO($request->validated())) 
+       ? response()->json(["Response" => "Код совпадает"])
+       : response()->json(["Response" => "Код НЕ совпадает"]);
+       
     }
 }
